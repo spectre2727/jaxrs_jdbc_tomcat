0145b8be7d0882c0a65291b71e8c23dc9eee97c8
spectral27 <gsoldano27@aol.com>
2021-12-30 01:43:36 +0100

    Update

--- src/test/java/jaxrs_tomcat/controller/ItemControllerTest.java
+++ src/test/java/jaxrs_tomcat/controller/ItemControllerTest.java
@@ -5,9 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -41,20 +44,49 @@ public class ItemControllerTest extends JerseyTest {
 	
 	@Test
 	public void selectAllItemsTest() {
-		Item item = new Item();
-		item.setId("1");
-		item.setValue("testValue");
-		
-		List<Item> items = new ArrayList<Item>(Arrays.asList(item));
+		List<Item> items = new ArrayList<Item>(Arrays.asList(new Item()));
 		
 		Mockito.when(itemRepository.selectAllItems()).thenReturn(items);
 		
 		Response response = target("/items").request().get();
+		
 		List<Item> responseItems = response.readEntity(new GenericType<List<Item>>() {});
 		
 		assertEquals(Status.OK.getStatusCode(), response.getStatus());
 		assertEquals(1, responseItems.size());
-		assertEquals("testValue", responseItems.get(0).getValue());
+	}
+	
+	@Test
+	public void insertItemTest() {
+		Mockito.when(itemRepository.insertItem(Mockito.any())).thenReturn(1);
+		
+		Response response = target("/items").request()
+				.post(Entity.entity(new Item(), MediaType.APPLICATION_JSON));
+		
+		assertEquals(Status.OK.getStatusCode(), response.getStatus());
+		assertEquals(1, response.readEntity(Integer.class));
+	}
+	
+	@Test
+	public void updateItemTest() {
+		Mockito.when(itemRepository.updateItem(Mockito.anyString(), Mockito.any())).thenReturn(1);
+		
+		Response response = target("/items" + "/" + new Random().nextInt(10) + 1).request()
+				.put(Entity.entity(new Item(), MediaType.APPLICATION_JSON));
+		
+		assertEquals(Status.OK.getStatusCode(), response.getStatus());
+		assertEquals(1, response.readEntity(Integer.class));
+	}
+	
+	@Test
+	public void deleteItemTest() {
+		Mockito.when(itemRepository.deleteItem(Mockito.anyString())).thenReturn(1);
+		
+		Response response = target("/items" + "/" + new Random().nextInt(10) + 1).request()
+				.delete();
+		
+		assertEquals(Status.OK.getStatusCode(), response.getStatus());
+		assertEquals(1, response.readEntity(Integer.class));
 	}
 
 }
--- src/test/java/jaxrs_tomcat/repository/ItemRepositoryTest.java
+++ src/test/java/jaxrs_tomcat/repository/ItemRepositoryTest.java
@@ -1,8 +1,9 @@
 package jaxrs_tomcat.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,9 +11,10 @@ import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -37,63 +39,109 @@ public class ItemRepositoryTest {
 	private ItemRepository itemRepository;
 
 	@BeforeAll
-	public static void initConnection() throws SQLException {
+	public static void beforeTests() throws SQLException {
 		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
 		connection.createStatement().executeQuery("create table items (id varchar(16), value varchar(50))");
-		connection.createStatement().executeQuery("insert into items values (\'1\', \'value1\')");
+		connection.createStatement().executeQuery("insert into items values (\'toSelect\', \'value\')");
+		connection.createStatement().executeQuery("insert into items values (\'toUpdate\', \'value\')");
+		connection.createStatement().executeQuery("insert into items values (\'toDelete\', \'value\')");
+		
+		System.setErr(new PrintStream(new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+				
+			}
+		}));
 	}
 	
-	@BeforeEach
-	public void initRepository() throws SQLException {
+	@AfterAll
+	public static void afterTests() {
 		System.setErr(System.err);
-		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
-		Mockito.when(jdbcConnection.get()).thenReturn(connection);
 	}
 
 	@Test
 	public void selectAllItemsTest() throws SQLException {
-		assertNotNull(itemRepository.selectAllItems().get(0));
+		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
+		Mockito.when(jdbcConnection.get()).thenReturn(connection);
+		
+		List<Item> items = itemRepository.selectAllItems();
+		
+		assertNotNull(items);
+		assertTrue(items.size() > 0);
 	}
 
 	@Test
-	public void dontSelectAllItemsTest() {
-		PrintStream noPrintStream = new PrintStream(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {
-				
-			}
-		});
-		System.setErr(noPrintStream);
-		
-		BDDMockito.given(jdbcConnection.get()).willAnswer(i -> {
+	public void selectAllItemsExceptionTest() {
+		BDDMockito.given(jdbcConnection.get()).willAnswer(answer -> {
 			throw new SQLException();
 		});
+			
+		List<Item> items = itemRepository.selectAllItems();
 		
-		assertNull(itemRepository.selectAllItems());
+		assertNull(items);
 	}
 	
 	@Test
 	public void insertItemTest() throws SQLException {
-		Item item = new Item();
-		item.setValue("itemValue");
-		assertEquals(1, itemRepository.insertItem(item));
+		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
+		Mockito.when(jdbcConnection.get()).thenReturn(connection);
+		
+		int insertResult = itemRepository.insertItem(new Item());
+		
+		assertEquals(1, insertResult);
 	}
 	
 	@Test
-	public void dontInsertItemTest() {
-		PrintStream noPrintStream = new PrintStream(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {
-				
-			}
-		});
-		System.setErr(noPrintStream);
-		
-		BDDMockito.given(jdbcConnection.get()).willAnswer(i -> {
+	public void insertItemExceptionTest() {
+		BDDMockito.given(jdbcConnection.get()).willAnswer(answer -> {
 			throw new SQLException();
 		});
 		
-		assertEquals(0, itemRepository.insertItem(new Item()));
+		int insertResult = itemRepository.insertItem(new Item());
+		
+		assertEquals(0, insertResult);
+	}
+	
+	@Test
+	public void updateItemTest() throws SQLException {
+		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
+		Mockito.when(jdbcConnection.get()).thenReturn(connection);
+		
+		int updateResult = itemRepository.updateItem("toUpdate", new Item());
+		
+		assertEquals(1, updateResult);
+	}
+	
+	@Test
+	public void updateItemExceptionTest() {
+		BDDMockito.given(jdbcConnection.get()).willAnswer(answer -> {
+			throw new SQLException();
+		});
+		
+		int updateResult = itemRepository.updateItem("toUpdate", new Item());
+		
+		assertEquals(0, updateResult);
+	}
+	
+	@Test
+	public void deleteItemTest() throws SQLException {
+		connection = DriverManager.getConnection("jdbc:hsqldb:mem:standard", "root", "root");
+		Mockito.when(jdbcConnection.get()).thenReturn(connection);
+		
+		int deleteResult = itemRepository.deleteItem("toDelete");
+		
+		assertEquals(1, deleteResult);
+	}
+	
+	@Test
+	public void deleteItemExceptionTest() {
+		BDDMockito.given(jdbcConnection.get()).willAnswer(answer -> {
+			throw new SQLException();
+		});
+		
+		int deleteResult = itemRepository.deleteItem("toDelete");
+		
+		assertEquals(0, deleteResult);
 	}
 
 }
